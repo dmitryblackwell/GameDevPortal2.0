5886317a5094c01e804ef95f0063eee30eb070b3
dmitryblackwell <dmitryblackwell@gmail.com>
2018-05-29 15:15:10 +0300

    New icon
    Add new awesome icon to PI_ZNO_score project. Looks awesome.

--- PI_ZNO_score/gears/links.php
+++ PI_ZNO_score/gears/links.php
@@ -3,3 +3,4 @@
 <link rel="stylesheet" href="styles/ie.css" type="text/css" media="screen, projection">
 <![endif]-->
 <link rel="stylesheet" type="text/css" media="screen" href="styles/style.css">
+<link rel="shortcut icon" href="img/uicon.png" />
